9dea03cc5deac220f3f1a71289b0be709fe50c28
Rachit Agarwal <rachitagarwal0202@gmail.com>
2019-02-22 01:55:14 +0100

    started hmm

--- src/model/HMMLocalizer.java
+++ src/model/HMMLocalizer.java
@@ -9,25 +9,35 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.ejml.data.Matrix;
+import org.ejml.simple.SimpleMatrix;
+import org.ejml.data.DMatrix;
+import org.ejml.data.DMatrixRMaj;
+import org.ejml.data.DenseD2Matrix64F;
+
 public class HMMLocalizer implements EstimatorInterface {
 
 	private int rows, cols, heads;
 	private int heading;
 	private int[] position = new int[2];
 	private Random numGenerator;
-	
+
 	// North, East, South, West
-	private static final int[][] HEADINGS = new int[][] { { -1,0 }, { 0,1 }, { 1,0 }, { 0,-1 } };
-	
-	private static final int[][] PRIMARY_RING = new int[][] { {-1,-1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1} };
-	private static final int[][] SECONDARY_RING = new int[][] { {-2,-2}, {-2, -1}, {-2, 0}, {-2, 1}, {-2, 2}, {-1, 2}, {0, 2}, {1, 2}, {2, 2}, {2, 1}, {2, 0}, {2, -1}, {2, -2}, {1, -2}, {0, -2}, {-1, -2} };
-	
+	private static final int[][] HEADINGS = new int[][] { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
+
+	private static final int[][] PRIMARY_RING = new int[][] { { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, 1 }, { 1, 1 },
+			{ 1, 0 }, { 1, -1 }, { 0, -1 } };
+	private static final int[][] SECONDARY_RING = new int[][] { { -2, -2 }, { -2, -1 }, { -2, 0 }, { -2, 1 }, { -2, 2 },
+			{ -1, 2 }, { 0, 2 }, { 1, 2 }, { 2, 2 }, { 2, 1 }, { 2, 0 }, { 2, -1 }, { 2, -2 }, { 1, -2 }, { 0, -2 },
+			{ -1, -2 } };
+
 	private HashMap<int[], double[][]> observationMatrices;
 	private HashMap<int[], double[][]> nothingMatrices;
-	
+
 	private double[][] transitionMatrix;
-	private double[] hmmMatrix;
-	
+	private double[] hmmArray;
+	private double[][] hmmMatrix;
+
 	public HMMLocalizer(int rows, int cols, int heads) {
 		this.rows = rows;
 		this.cols = cols;
@@ -35,68 +45,75 @@ public class HMMLocalizer implements EstimatorInterface {
 
 		position[0] = 0;
 		position[1] = 0;
-		
+
 		numGenerator = new Random();
 		heading = numGenerator.nextInt(4);
-		
+
 		initializeObservationMatrices();
-		initializeHMMMatrix();
+		initializeHMM();
 		initializeTransitionMatrix();
 	}
-	
+
 	private void initializeObservationMatrices() {
 		observationMatrices = new HashMap<int[], double[][]>();
-		
+
 		for (int matrixRowIndex = 0; matrixRowIndex < rows; matrixRowIndex++) {
 			for (int matrixColIndex = 0; matrixColIndex < cols; matrixColIndex++) {
 				double[][] newMatrix = new double[rows][cols];
 
 				for (int row = 0; row < rows; row++) {
 					for (int col = 0; col < cols; col++) {
-						double distanceSquared = Math.pow(row - matrixRowIndex, 2) + Math.pow(col - matrixColIndex,  2);
-						int distance =  (int) Math.sqrt(distanceSquared);
-						
-						if (distance == 0) { newMatrix[row][col] = 0.1; }
-						else if (distance == 1) { newMatrix[row][col] = 0.05; }
-						else if (distance == 2) { newMatrix[row][col] = 0.025; }
-						else { newMatrix[row][col] = 0.0; }
+						double distanceSquared = Math.pow(row - matrixRowIndex, 2) + Math.pow(col - matrixColIndex, 2);
+						int distance = (int) Math.sqrt(distanceSquared);
+
+						if (distance == 0) {
+							newMatrix[row][col] = 0.1;
+						} else if (distance == 1) {
+							newMatrix[row][col] = 0.05;
+						} else if (distance == 2) {
+							newMatrix[row][col] = 0.025;
+						} else {
+							newMatrix[row][col] = 0.0;
+						}
 					}
 				}
-				observationMatrices.put(new int[] {matrixRowIndex,  matrixColIndex}, newMatrix);
+				observationMatrices.put(new int[] { matrixRowIndex, matrixColIndex }, newMatrix);
 			}
 		}
-		
+
 		// for (int[] key : observationMatrices.keySet()) {
-		// 	System.out.println(String.format("Matrix for (%d, %d)", key[0], key[1])); 
-		// 	double[][] matrix = observationMatrices.get(key);
-		// 	for (int i = 0; i< matrix.length; i++) {
-		// 	    for (int j = 0; j < matrix[i].length; j++) {
-		// 	        System.out.print(matrix[i][j] + "  ");
-		// 	    }
-		// 	        System.out.println();
-		// 	}
+		// System.out.println(String.format("Matrix for (%d, %d)", key[0], key[1]));
+		// double[][] matrix = observationMatrices.get(key);
+		// for (int i = 0; i< matrix.length; i++) {
+		// for (int j = 0; j < matrix[i].length; j++) {
+		// System.out.print(matrix[i][j] + " ");
+		// }
+		// System.out.println();
+		// }
 		// }
 	}
-	
-	private void initializeHMMMatrix() {
-		hmmMatrix = new double[rows * cols * heads];
+
+	private void initializeHMM() {
+		hmmArray = new double[rows * cols * heads];
+		hmmArray[position[0] * rows + position[1] * cols + heading] = 1;
+		hmmMatrix = new double[rows][cols];
 	}
-	
+
 	private void initializeTransitionMatrix() {
 		transitionMatrix = new double[rows * cols * heads][rows * cols * heads];
 
 		for (int i = 0; i < transitionMatrix.length; i++) {
 			int[] iVals = getValsFromIndex(i);
-//			System.out.println("Index " + i + ": "+ Arrays.toString(iVals));
+			// System.out.println("Index " + i + ": "+ Arrays.toString(iVals));
 			for (int j = 0; j < transitionMatrix[i].length; j++) {
 				int[] jVals = getValsFromIndex(j);
 				transitionMatrix[i][j] = getTProb(iVals[0], iVals[1], iVals[2], jVals[0], jVals[1], jVals[2]);
 			}
 		}
-		
-		printMatrix(transitionMatrix);
+
+		// printMatrix(transitionMatrix);
 	}
-	
+
 	private void printMatrix(double[][] array) {
 		for (int x = 0; x < array.length; x++) {
 			for (int y = 0; y < array[x].length; y++) {
@@ -105,24 +122,24 @@ public class HMMLocalizer implements EstimatorInterface {
 			System.out.println();
 		}
 	}
-	
-//	private int getIndexFromRowColHead(int row, int col, int head) {
-//		int start = row * (rows * heads);
-//		start += col * heads;
-//		start += head;
-//		return start;	
-//	}
-//	
+
+	// private int getIndexFromRowColHead(int row, int col, int head) {
+	// int start = row * (rows * heads);
+	// start += col * heads;
+	// start += head;
+	// return start;
+	// }
+	//
 	private int[] getValsFromIndex(int index) {
 		int head = index % heads;
 		index -= head;
-		
+
 		int col = (index % (rows * heads)) / heads;
 		index -= (col * heads);
-		
+
 		int row = index / rows;
-		
-		return new int[] {row, col, head};
+
+		return new int[] { row, col, head };
 	}
 
 	@Override
@@ -157,22 +174,39 @@ public class HMMLocalizer implements EstimatorInterface {
 				headingsToChoose.add(headingVal);
 			}
 		}
-		
+
 		numGenerator = new Random();
 		int nextHeadingIndex = numGenerator.nextInt(headingsToChoose.size());
 
 		int nextHeading = headingsToChoose.get(nextHeadingIndex);
 		heading = nextHeading;
-		
+
 		position[0] = position[0] + HEADINGS[heading][0];
 		position[1] = position[1] + HEADINGS[heading][1];
-		
+
 		System.out.println(String.format("New Position: (%d, %d)", position[0], position[1]));
 	}
 
-	
 	private void updateHmmMatrix() {
+		double[][] transposedTransitionMatrix = transposeMatrix(transitionMatrix);
+		double[][] diagMatrix = getDiagonalMatrix();
+		double[][] multipliedTranspose = multiply(diagMatrix, transposedTransitionMatrix);
+		double[] newHmm = multiply(multipliedTranspose, hmmArray);
+		hmmArray = newHmm;
 		
+	}
+
+	private double[][] getDiagonalMatrix() {
+		int len = rows * cols * heads;
+		double[][] diagMatrix = new double[len][len];
+		for (int i = 0; i < len; i++) {
+			for (int j = 0; j < len; j++) {
+				if (i == j)
+					diagMatrix[i][j] = 1;
+			}
+		}
+
+		return diagMatrix;
 	}
 
 	private HashMap<Integer, Double> getProbMap(int row, int col, int head) {
@@ -200,7 +234,7 @@ public class HMMLocalizer implements EstimatorInterface {
 
 		return probMap;
 	}
-		
+
 	private ArrayList<Integer> getPossibleHeadings(int row, int col) {
 		ArrayList<Integer> possibleHeadings = new ArrayList<Integer>();
 		possibleHeadings.addAll(Arrays.asList(0, 1, 2, 3));
@@ -213,61 +247,70 @@ public class HMMLocalizer implements EstimatorInterface {
 			possibleHeadings.remove(new Integer(3)); // remove west
 		if (col == cols - 1)
 			possibleHeadings.remove(new Integer(1)); // remove east
-		
-//		System.out.println(String.format("Possible Headings for %d, %d: %s", row, col, possibleHeadings.toString()));
-		
+
+		// System.out.println(String.format("Possible Headings for %d, %d: %s", row,
+		// col, possibleHeadings.toString()));
+
 		return possibleHeadings;
 	}
 
 	@Override
 	public int[] getCurrentTrueState() {
-		return new int[] {position[0], position[1], heading};
+		return new int[] { position[0], position[1], heading };
 	}
 
 	@Override
 	public int[] getCurrentReading() {
 		numGenerator = new Random();
-		
+
 		ArrayList<int[]> possibleReadings = new ArrayList<int[]>();
-		
-		for (int i = 0; i < 4; i++) possibleReadings.add(position);
+
+		for (int i = 0; i < 4; i++)
+			possibleReadings.add(position);
 		for (int i = 0; i < 2; i++) {
-			for (int j = 0; j < PRIMARY_RING.length; j++) possibleReadings.add(PRIMARY_RING[j]);
+			for (int j = 0; j < PRIMARY_RING.length; j++)
+				possibleReadings.add(PRIMARY_RING[j]);
 		}
-		for (int i = 0; i < SECONDARY_RING.length; i++) possibleReadings.add(SECONDARY_RING[i]);
-		
+		for (int i = 0; i < SECONDARY_RING.length; i++)
+			possibleReadings.add(SECONDARY_RING[i]);
+
 		// possibleReadings List should be of size 36 now
-		
+
 		int randNum = numGenerator.nextInt(40);
-		
-		if (randNum >= 36) return null;
-		if (randNum < 4) return position;
-		
+
+		if (randNum >= 36)
+			return null;
+		if (randNum < 4)
+			return position;
+
 		int[] ringCoordShift = possibleReadings.get(randNum);
 		int[] newRingCoord = new int[] { position[0] + ringCoordShift[0], position[1] + ringCoordShift[1] };
-		
+
 		if (inBounds(newRingCoord[0], newRingCoord[1])) {
 			return newRingCoord;
 		}
-		
-		return null;		
+
+		return null;
 	}
 
 	@Override
 	public double getCurrentProb(int x, int y) {
 		return 0.0;
-//		return hmmMatrix[x][y];
+		// return hmmMatrix[x][y];
 	}
 
 	@Override
 	public double getOrXY(int rX, int rY, int x, int y, int h) {
-		double distanceSquared = Math.pow(x - rX,  2) + Math.pow(y - rY,  2);
+		double distanceSquared = Math.pow(x - rX, 2) + Math.pow(y - rY, 2);
 		int distance = (int) Math.sqrt(distanceSquared);
-		
-		if (distance == 0) return 0.1;
-		if (distance == 1) return 0.05;
-		if (distance == 2) return 0.025;
-		
+
+		if (distance == 0)
+			return 0.1;
+		if (distance == 1)
+			return 0.05;
+		if (distance == 2)
+			return 0.025;
+
 		return 0.0;
 	}
 
@@ -275,26 +318,71 @@ public class HMMLocalizer implements EstimatorInterface {
 	public double getTProb(int x, int y, int h, int nX, int nY, int nH) {
 		int dx = nX - x;
 		int dy = nY - y;
-		
+
 		HashMap<Integer, Double> probMap = getProbMap(x, y, h);
-		
+
 		if (probMap.containsKey(nH) && HEADINGS[nH][0] == dx && HEADINGS[nH][1] == dy) {
-//				System.out.println("---T PROB ---");
-//				System.out.println(String.format("(x, y, h) -- (%d, %d, %d)", x, y, h));
-//				System.out.println(String.format("(nX, nY, nH) -- (%d, %d, %d)", nX, nY, nH));		
-//				System.out.println("Prob Map: " + probMap.toString());
-				return probMap.get(nH);
+			// System.out.println("---T PROB ---");
+			// System.out.println(String.format("(x, y, h) -- (%d, %d, %d)", x, y, h));
+			// System.out.println(String.format("(nX, nY, nH) -- (%d, %d, %d)", nX, nY,
+			// nH));
+			// System.out.println("Prob Map: " + probMap.toString());
+			return probMap.get(nH);
 		}
-		
+
 		return 0.0;
 	}
-	
+
 	private boolean inBounds(int row, int col) {
 		return row >= 0 && row < rows && col >= 0 && col < cols;
 	}
-	
+
 	private boolean newLocationInBounds(int row, int col, int dRow, int dCol) {
-		return inBounds (row + dRow, col + dCol);
+		return inBounds(row + dRow, col + dCol);
+	}
+
+	// The following 3 matrix methods included under fair use guidelines
+	// to prevent needing to use a matrix library
+	// https://introcs.cs.princeton.edu/java/22library/Matrix.java.html
+
+	// From https://introcs.cs.princeton.edu/java/22library/Matrix.java.html
+	public static double[][] transposeMatrix(double[][] a) {
+		int m = a.length;
+		int n = a[0].length;
+		double[][] b = new double[n][m];
+		for (int i = 0; i < m; i++)
+			for (int j = 0; j < n; j++)
+				b[j][i] = a[i][j];
+		return b;
+	}
+
+	// From https://introcs.cs.princeton.edu/java/22library/Matrix.java.html
+	public static double[][] multiply(double[][] a, double[][] b) {
+		int m1 = a.length;
+		int n1 = a[0].length;
+		int m2 = b.length;
+		int n2 = b[0].length;
+		if (n1 != m2)
+			throw new RuntimeException("Illegal matrix dimensions.");
+		double[][] c = new double[m1][n2];
+		for (int i = 0; i < m1; i++)
+			for (int j = 0; j < n2; j++)
+				for (int k = 0; k < n1; k++)
+					c[i][j] += a[i][k] * b[k][j];
+		return c;
+	}
+
+	// From https://introcs.cs.princeton.edu/java/22library/Matrix.java.html
+	public static double[] multiply(double[][] a, double[] x) {
+		int m = a.length;
+		int n = a[0].length;
+		if (x.length != n)
+			throw new RuntimeException("Illegal matrix dimensions.");
+		double[] y = new double[m];
+		for (int i = 0; i < m; i++)
+			for (int j = 0; j < n; j++)
+				y[i] += a[i][j] * x[j];
+		return y;
 	}
 
 }
